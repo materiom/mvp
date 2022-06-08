300d7dad45fb59a95bf450624dc83bcefe2d8c5f
liamreeves <liamreeves42@gmail.com>
2022-06-08 17:04:19 +0100

    First pass at new login and register designs

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,22 @@
 import React from "react";
-import ArrowButton from "./ArrowButton";
-import GoogleLogin from "./GoogleLoginButton";
-import TwitterLoginButton from "./TwitterLoginButton";
+import { Link } from "react-router-dom";
+import CustomButton from "./CustomButton";
 
 function LoginForm() {
   return (
-    <div className="flex flex-col items-center justify-between p-5 h-[80vh] w-[80%] max-w-[500px]">
-      <h1 className="text-3xl my-5">Sign in to Materiom</h1>
-      <div className="flex flex-col items-center h-full w-full">
-        <div className="flex flex-row justify-between items-center w-full">
-          <GoogleLogin />
-          <TwitterLoginButton />
-        </div>
+    <div
+      className={`flex flex-col items-center justify-between p-5 
+    w-[80%] max-w-[500px] rounded-[10px] absolute
+     top-[25%] left-[45%] shadow-2xl bg-white`}
+    >
+      <div className="text-center">
+        <h1 className="text-3xl my-5 ">Get Started</h1>
+        <Link className=" no-underline" to="/register">
+          Register
+        </Link>
+      </div>
+      <div className="flex flex-col items-center justify-center h-full w-full">
+        <div className="flex flex-row justify-center items-center w-full"></div>
         <h3 className=" mr-auto my-5">OR</h3>
         <form className="h-full flex flex-col  w-full">
           <div className="flex flex-col pb-5">
@@ -26,7 +31,8 @@ function LoginForm() {
             <input type="password" />
           </div>
 
-          <ArrowButton
+          <CustomButton
+            style={{ margin: "auto" }}
             displayText="Login"
             function={() => alert("Working on it :)")}
             color="blue"
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,52 +1,56 @@
 import React from "react";
-import ArrowButton from "./ArrowButton";
+import { Link } from "react-router-dom";
+import CustomButton from "./CustomButton";
 
 function RegistrationForm() {
   return (
-    <div className="flex flex-col items-center justify-between p-5 h-[80vh] max-w-[400px]">
-      <h1 className="text-3xl my-5">Register for Materiom</h1>
+    <div
+      className={`flex flex-col items-center justify-between p-5 
+     w-[80%] max-w-[500px] rounded-[10px] absolute
+     top-[25%] left-[45%] shadow-2xl bg-white`}
+    >
+      <div className="text-center">
+        <h1 className="text-3xl my-5">Get started</h1>
+        <Link className=" no-underline" to="/login">
+          Login
+        </Link>
+      </div>
       <div className="flex flex-col items-center h-full w-full">
-        <div className="flex flex-row justify-between items-center w-full">
-          <button className="bg-blue-400 rounded p-3 text-xs">
-            Sign up with Google
-          </button>
-          <button className="bg-blue-800 rounded p-3 text-xs">
-            Sign up with Twitter
-          </button>
-        </div>
+        <div className="flex flex-row justify-center items-center w-full"></div>
         <h3 className=" mr-auto my-5">OR</h3>
         <form className="h-full flex flex-col justify-around w-full">
-          <div className="flex flex-col pb-5">
+          <div className="flex flex-col pb-3">
             <label htmlFor="">Name</label>
             <input type="text" />
           </div>
-          <div className="flex flex-col pb-5">
-            <label htmlFor="">Username</label>
+          <div className="flex flex-col pb-3">
+            <label htmlFor="">Email</label>
             <input type="email" />
           </div>
-          <div className="flex flex-col pb-5">
+          <div className="flex flex-col pb-3">
             <label htmlFor="">Password</label>
             <input type="password" />
           </div>
-          <div className="flex flex-col pb-5">
+          <div className="flex flex-col pb-3">
             <label htmlFor="">Confirm Password</label>
             <input type="password" />
           </div>
-          <div className="flex flex-row-reverse pb-5">
+          <div className="flex flex-row-reverse pb-3">
             <label htmlFor="term" className=" text-xs">
               {" "}
               Creating an account means you are okay with our{" "}
               <a href="#">Terms of Service</a>, <a href="#"> Privacy Policy</a>{" "}
               and out default <a href="">Notification Setting</a>
             </label>
-            <input type="checkbox" className="custom-checkbox"/>
+            <input type="checkbox" className="custom-checkbox" />
           </div>
 
-          <ArrowButton
-              displayText="Register"
-              function={() => alert("Working on it :)")}
-              color="blue"
-            />
+          <CustomButton
+            style={{ margin: "auto" }}
+            displayText="Register"
+            function={() => alert("Working on it :)")}
+            color="blue"
+          />
         </form>
       </div>
     </div>
