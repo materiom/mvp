579efb5d42063551289f3b3e5dedb64df7d90f63
liamreeves <liamreeves42@gmail.com>
2022-06-27 16:50:00 +0100

    User directory indvidual user page

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import EditProfile from "./routes/EditProfile";
 import Contribute from "./routes/Contribute";
 import UserDirectory from "./routes/UserDirectory";
 import DataCommons from "./routes/DataCommons";
+import UserProfile from "./routes/UserProfile";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -28,6 +29,9 @@ root.render(
           <Route path="edit-profile" element={<EditProfile title="Edit Profile - Materiom" />} />
           <Route path="connect" element={<UserDirectory title="User Directory - Materiom" />} />
           <Route path="analyse" element={<DataCommons title="User Directory - Materiom" />} />
+          <Route path="user-profile">
+            <Route path=":userId" element={<UserProfile title="Profile - Materiom" />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </Provider>
